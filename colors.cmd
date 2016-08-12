199b17ea3361a6f119988c72399a1ebc532b02bd
Braydon Temple <roadyjr@yahoo.com>
2016-08-11 21:38:23 -0400

    Add files via upload
    for the first directory change, find a directory with a decent amount of stuff (like the folder java is installed in)
    the second directory change needs to be where the cmd file is located

---
+++ colors.cmd
@@ -0,0 +1,21 @@
+cd C:\Program Files (x86)\Java
+color a
+tree
+color b
+tree
+color c
+tree
+color d
+tree
+color 1
+tree
+color 2
+tree
+color 3
+tree
+color 4
+tree
+color 5
+tree
+cd C:\Users\222008831\Desktop
+colors.cmd
